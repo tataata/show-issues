39309701bcbe132460b82c27f64c8a01d63b3ee1
tataata <fortataata@gmail.com>
2017-10-10 14:14:54 +0200

    add jquery; add conuter of the open issues for the repo in list

--- src/components/repositories.js
+++ src/components/repositories.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import { UserCard } from './user_card.js';
 
 class Repositories extends React.Component {
   render() {
     const repos = this.props.chocolate;
     const repoItems = repos.map(repo => {
       return (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>{repo.name} ({repo.open_issues_count})</li>
       )
     });
 
